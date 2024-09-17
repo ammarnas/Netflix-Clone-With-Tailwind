1d753a2eae333f2c5a9b8d52f255140bd55407ce
ammarnas <ammarnasser.contact@gmail.com>
2024-09-17 22:56:43 +0300

    Angular Social Login with Google

---
+++ src/app/browse/browse.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-browse',
+  standalone: true,
+  imports: [],
+  templateUrl: './browse.component.html',
+  styleUrl: './browse.component.scss'
+})
+export class BrowseComponent {
+  authService = inject(AuthService);
+  name = JSON.parse(sessionStorage.getItem("loggedInUser")!).name;
+  userProfileImg = JSON.parse(sessionStorage.getItem("loggedInUser")!).picture;
+  email = JSON.parse(sessionStorage.getItem("loggedInUser")!).email;
+  singOut() {
+    sessionStorage.removeItem("loggedInUser")
+    this.authService.signOut();
+  }
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,47 @@
+declare var google: any;
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent implements OnInit {
+
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    google.accounts.id.initialize({
+      client_id: '22632047741-hp6fqmual45k7t5cmlgdskpu55sng7pv.apps.googleusercontent.com',
+      callback: (resp: any) =>{
+        this.handleLogin(resp);
+        console.log(resp)
+      }
+});
+
+  google.accounts.id.renderButton(document.getElementById("google-btn"), {
+    theme: 'filled_blue',
+    size: 'large',
+    shape: 'rectangle',
+    width: 350
+  })
+  }
+
+  handleLogin(response: any){
+    if(response){
+      // decode token
+      const payload = this.decodeToken(response.credential)
+      // store in session
+      sessionStorage.setItem("loggedInUser", JSON.stringify(payload));
+      // redirect to home/browse
+      this.router.navigate(['browse']);
+    }
+  }
+
+  private decodeToken(token: string) {
+    return JSON.parse(atob(token.split(".")[1]))
+  }
+}
