8390017142353ca2657ae5e06f85b86b2e8133b0
ammarnas <ammarnasser.contact@gmail.com>
2024-09-18 20:52:38 +0300

    finish header with add Welcome User + Profile Image + Sing Out

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @Input({required: true}) userImage : string = '';
   navList = ['Home', 'TV Shows', 'News & Popular', 'My List', 'Browse by Language']
 }
