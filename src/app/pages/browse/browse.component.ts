d57ec3e175c78f280e2a0873056fb155bd8f1e3a
ammarnas <ammarnasser.contact@gmail.com>
2024-09-18 19:34:04 +0300

    create structure

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { AuthService } from '../auth.service';
+import { AuthService } from '../../shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-browse',
